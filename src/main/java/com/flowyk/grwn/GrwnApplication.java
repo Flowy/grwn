548404d49566765ebac61117db964b49c3864d15
Lukas <flowyk@gmail.com>
2020-10-14 11:48:01 +0200

    Spring Boot

---
+++ src/main/java/com/flowyk/grwn/GrwnApplication.java
@@ -0,0 +1,13 @@
+package com.flowyk.grwn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrwnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrwnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flowyk/grwn/GrwnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowyk.grwn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrwnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
